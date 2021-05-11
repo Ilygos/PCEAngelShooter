ddd41ff01facc1fb51097d9f7b18ef7ab8d8340d
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-05-11 22:15:06 +0200

    [BACKGROUND] Adding background

--- main.c
+++ main.c
@@ -6,6 +6,21 @@ int TimerCnt;
 int MainCnt;
 int SubCnt;
 
+
+#incchr(tiles_decor, "background.pcx", 32, 29);
+#incbat(map_decor, "background.pcx", 0x1000, 32, 29);
+#incpal(palette_decor, "background.pcx");
+
 void main(void)
 {
+    vsync(1);    
+    set_xres(256);
+    init_satb();
+    load_background(tiles_decor, palette_decor, map_decor, 32, 29);
+    satb_update();
+
+    for (;;)
+    {
+        
+    }
 }
