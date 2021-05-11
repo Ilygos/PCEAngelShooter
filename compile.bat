7fe34a9bbc85f3ec6adaebebf649e1eb50c919f7
Maxence Beaumont <m.beaumont@ohbibi.com>
2021-05-11 21:35:55 +0200

    [BAT] Adding some bat files allowing to optimize compiling and launching of the game

---
+++ compile.bat
@@ -0,0 +1,19 @@
+cls
+@echo off
+
+SET EXTENSION=%~x1
+SET NAME=%1
+SET CHEMIN=%~dp0
+SET PCE_INCLUDE=%CHEMIN%include\pce
+
+If "%EXTENSION%"==".c" goto Compilc
+If "%EXTENSION%"==".C" goto Compilc
+:Compilasm
+bin\pceas.exe %1
+goto Fin
+
+:Compilc
+bin\huc.exe %1
+bin\pceas.exe -s %NAME:~0,-2%.s  
+
+:Fin     
